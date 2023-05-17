60a63b2af4b41b7a2f45324217bc9229d8826f39
rishi <rishi93dev@gmail.com>
2023-05-17 11:21:46 +0200

    Add AstPrinter functionality

---
+++ AstPrinter.java
@@ -0,0 +1,65 @@
+/*
+    When we debug our parser and interpreter, it's often useful to look at a
+    parsed syntax tree and make sure it has the structure we expect.
+
+    We'd like some code that, given a syntax tree, produces an unambigious
+    string representation of it. Converting a tree to a string is sort of
+    opposite of a parser, and is often called "pretty printing"
+
+    The string representation we produce will look a lot like, Lisp.
+    Each expression is explicitly parenthesized, and all of it's subexpressions
+    and tokens are contained in it.
+*/
+package lox;
+
+class AstPrinter implements Expr.Visitor<String> {
+    String print(Expr expr) {
+        return expr.accept(this);
+    }
+
+    @Override
+    public String visitBinaryExpr(Expr.Binary expr) {
+        return parenthesize(expr.operator.lexeme, expr.left, expr.right);
+    }
+
+    @Override
+    public String visitGroupingExpr(Expr.Grouping expr) {
+        return parenthesize("group", expr.expression);
+    }
+
+    @Override
+    public String visitLiteralExpr(Expr.Literal expr) {
+        if(expr.value == null) return "nil";
+        return expr.value.toString();
+    }
+
+    @Override
+    public String visitUnaryExpr(Expr.Unary expr) {
+        return parenthesize(expr.operator.lexeme, expr.right);
+    }
+
+    private String parenthesize(String name, Expr...exprs) {
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("(").append(name);
+        for(Expr expr : exprs) {
+            builder.append(" ");
+            builder.append(expr.accept(this));
+        }
+        builder.append(")");
+
+        return builder.toString();
+    }
+
+    public static void main(String[] args) {
+        Expr expression = new Expr.Binary(
+            new Expr.Unary(
+                new Token(TokenType.MINUS, "-", null, 1),
+                new Expr.Literal(123)),
+            new Token(TokenType.STAR, "*", null, 1),
+            new Expr.Grouping(
+                new Expr.Literal(45.67)));
+
+        System.out.println(new AstPrinter().print(expression));
+    }
+}
