9dbe3a85b46f619b784b0c8ddac29b5547fba326
rishi <rishi93dev@gmail.com>
2023-05-17 11:21:30 +0200

    Add Expr classes to represent syntax tree nodes

---
+++ Expr.java
@@ -0,0 +1,80 @@
+package lox;
+
+import java.util.List;
+
+abstract class Expr {
+	interface Visitor<R> {
+		R visitBinaryExpr(Binary expr);
+		R visitGroupingExpr(Grouping expr);
+		R visitLiteralExpr(Literal expr);
+		R visitUnaryExpr(Unary expr);
+	}
+	static class Binary extends Expr {
+		//Fields
+		final Expr left;
+		final Token operator;
+		final Expr right;
+
+		//constructor
+		Binary(Expr left, Token operator, Expr right) {
+			this.left = left;
+			this.operator = operator;
+			this.right = right;
+		}
+
+		@Override
+		<R> R accept(Visitor<R> visitor) {
+			 return visitor.visitBinaryExpr(this);
+		}
+	}
+
+	static class Grouping extends Expr {
+		//Fields
+		final Expr expression;
+
+		//constructor
+		Grouping(Expr expression) {
+			this.expression = expression;
+		}
+
+		@Override
+		<R> R accept(Visitor<R> visitor) {
+			 return visitor.visitGroupingExpr(this);
+		}
+	}
+
+	static class Literal extends Expr {
+		//Fields
+		final Object value;
+
+		//constructor
+		Literal(Object value) {
+			this.value = value;
+		}
+
+		@Override
+		<R> R accept(Visitor<R> visitor) {
+			 return visitor.visitLiteralExpr(this);
+		}
+	}
+
+	static class Unary extends Expr {
+		//Fields
+		final Token operator;
+		final Expr right;
+
+		//constructor
+		Unary(Token operator, Expr right) {
+			this.operator = operator;
+			this.right = right;
+		}
+
+		@Override
+		<R> R accept(Visitor<R> visitor) {
+			 return visitor.visitUnaryExpr(this);
+		}
+	}
+
+
+abstract <R> R accept(Visitor <R> visitor);
+}
